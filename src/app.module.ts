00b8f62d7ddd45593c7c72fd41532122822d29b9
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-21 19:10:53 +0100

    sequelize main configuration- add models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,32 @@
 import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { envs } from './config';
+import { Order } from './orders/entities/order.entity';
 
 @Module({
-    imports    : [OrdersModule],
+    imports: [
+        OrdersModule,
+        SequelizeModule.forRoot({
+            dialect       : envs.dialect,
+            host          : envs.dbHost,
+            port          : envs.dbPort,
+            username      : envs.dbUser,
+            password      : envs.dbPassword,
+            database      : envs.dbName,
+            autoLoadModels: envs.autoLoadModels,
+            synchronize   : envs.synchronize,
+            logging       : envs.logging ? console.log : false,
+            //ssl           : false, // Indica que SSL está habilitado
+            dialectOptions: {
+                ssl: {
+                    require           : true, // Requiere conexión SSL
+                    rejectUnauthorized: false, // Permite certificados autofirmados
+                },
+            },
+            models: [Order],
+        }),
+    ],
     controllers: [],
     providers  : [],
 })
