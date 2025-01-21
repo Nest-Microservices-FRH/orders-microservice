81ffc6fd3570094261fe7ec0b6fa83cd21a9cc0f
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-21 19:31:00 +0100

    find one necesary changes

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, NotImplementedException } from '@nestjs/common';
+import { Controller, NotImplementedException, ParseUUIDPipe } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
 
 @Controller()
 export class OrdersController {
     constructor(private readonly ordersService: OrdersService) {}
 
   @MessagePattern('createOrder')
-    create(@Payload() createOrderDto: CreateOrderDto) {
+    create(@Payload() createOrderDto: CreateOrderDto): Promise<Order> {
         return this.ordersService.create(createOrderDto);
     }
 
@@ -19,7 +20,7 @@ export class OrdersController {
   }
 
   @MessagePattern('findOneOrder')
-  findOne(@Payload() id: number) {
+  findOne(@Payload('id', ParseUUIDPipe) id: string): Promise<Order> {
       return this.ordersService.findOne(id);
   }
 
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Order } from './entities/order.entity';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService {
@@ -11,15 +12,24 @@ export class OrdersService {
         private orderModel: typeof Order,
     ) {}
 
-    create(createOrderDto: CreateOrderDto) {
+    create(createOrderDto: CreateOrderDto): Promise<Order> {
         return this.orderModel.create(createOrderDto);
     }
 
     findAll() {
-        return 'This action returns all ordersaaaaaa';
+        return 'This action returns all orders';
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} orderdsdsdsd`;
+    async findOne(id: string): Promise<Order> {
+        const order = await this.orderModel.findByPk(id);
+
+        if (!order) {
+            throw new RpcException({
+                status : HttpStatus.NOT_FOUND,
+                message: `Order with id ${id} not found`,
+            });
+        }
+
+        return order;
     }
 }
