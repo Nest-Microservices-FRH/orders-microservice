2b3d11f74ef92b5ba4821d7b4b810fe32c64a540
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-21 19:11:27 +0100

    model creation

---
+++ src/orders/entities/order.entity.ts
@@ -0,0 +1,72 @@
+import { DataTypes } from 'sequelize';
+import { Column, Model, Table } from 'sequelize-typescript';
+
+@Table({
+    modelName      : 'orders',
+    freezeTableName: true,
+    timestamps     : true,
+})
+export class Order extends Model<Order>{
+    @Column({
+        field       : 'id',
+        primaryKey  : true,
+        allowNull   : false,
+        type        : DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+    })
+        id: string;
+
+    @Column({
+        field       : 'totalAmount',
+        allowNull   : false,
+        type        : DataTypes.FLOAT,
+        defaultValue: 0.0,
+    })
+        totalAmount: number;
+
+    @Column({
+        field       : 'totalItems',
+        allowNull   : false,
+        type        : DataTypes.INTEGER, // Entero para la cantidad de artículos
+        defaultValue: 1,         // Valor predeterminado si no se especifica
+    })
+        totalItems: number;
+
+    @Column({
+        field       : 'status',
+        allowNull   : false,
+        type        : DataTypes.ENUM('PENDING', 'DELIVERED', 'CANCELLED'), // Valores aceptados
+        defaultValue: 'PENDING',                                  // Estado predeterminado
+    })
+        status: string;
+
+    @Column({
+        field       : 'paid',
+        allowNull   : false,
+        type        : DataTypes.BOOLEAN, // Indica si está pagado
+        defaultValue: false,     // Por defecto, no pagado
+    })
+        paid: boolean;
+
+    @Column({
+        field    : 'paidAt',
+        allowNull: true,
+        type     : DataTypes.DATE,
+    })
+        paidAt: string;
+
+    @Column({
+        field       : 'createdAt',
+        allowNull   : true,
+        type        : DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    })
+        createdAt: string;
+
+    @Column({
+        field    : 'updatedAt',
+        allowNull: true,
+        type     : DataTypes.DATE,
+    })
+        updatedAt: string;
+}
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Order } from './entities/order.entity';
 
 @Module({
     controllers: [OrdersController],
     providers  : [OrdersService],
+    imports    : [
+        SequelizeModule.forFeature([Order]),
+    ],
+    exports: [SequelizeModule],
 })
 export class OrdersModule {}
