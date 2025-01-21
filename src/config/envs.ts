e018995f86f74dd172a0a5e5c93cae918ef914fb
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-21 19:08:59 +0100

    config envs to use sequelize

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,13 +1,32 @@
 import 'dotenv/config';
 import * as joi from 'joi';
+import { Dialect } from 'sequelize';
 
 interface EnvVars {
   PORT: number;
+  DIALECT: Dialect;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_NAME: string;
+  DB_USER: string;
+  DB_PASSWORD: string;
+  LOGGING: boolean;
+  DB_SYNCHRONIZE: boolean;
+  DB_AUTOLOADMODELS: boolean;
 }
 
 const envsSchema = joi
     .object({
-        PORT: joi.number().required(),
+        PORT             : joi.number().required(),
+        DIALECT          : joi.string().required(),
+        DB_HOST          : joi.string().required(),
+        DB_PORT          : joi.number().required(),
+        DB_NAME          : joi.string().required(),
+        DB_USER          : joi.string().required(),
+        DB_PASSWORD      : joi.string().required(),
+        LOGGING          : joi.boolean().required(),
+        DB_SYNCHRONIZE   : joi.boolean().required(),
+        DB_AUTOLOADMODELS: joi.boolean().required(),
     })
     .unknown(true);
 
@@ -20,5 +39,14 @@ if (error) {
 const envVars: EnvVars = value;
 
 export const envs = {
-    port: envVars.PORT,
+    port          : envVars.PORT,
+    dialect       : envVars.DIALECT,
+    dbHost        : envVars.DB_HOST,
+    dbPort        : envVars.DB_PORT,
+    dbName        : envVars.DB_NAME,
+    dbUser        : envVars.DB_USER,
+    dbPassword    : envVars.DB_PASSWORD,
+    logging       : envVars.LOGGING,
+    synchronize   : envVars.DB_SYNCHRONIZE,
+    autoLoadModels: envVars.DB_AUTOLOADMODELS,
 };
