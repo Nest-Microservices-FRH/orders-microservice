2b9e7cfd2f84ae82dc0f1a96e0e20ccc273c4a99
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-02-07 12:07:25 +0100

    conection with products MS

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,12 +3,24 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Order } from './entities/order.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, PRODUCT_SERVICE } from 'src/config';
 
 @Module({
     controllers: [OrdersController],
     providers  : [OrdersService],
     imports    : [
         SequelizeModule.forFeature([Order]),
+        ClientsModule.register([
+            {
+                name     : PRODUCT_SERVICE,
+                transport: Transport.TCP,
+                options  : {
+                    host: envs.productsMsHost,
+                    port: envs.productsMsPort,
+                },
+            },
+        ]),
     ],
     exports: [SequelizeModule],
 })
